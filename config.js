538251b009fa93d3c58e0f325f028fccc4b18af4
nawoodanisanjila-ai <nawoodanisanjila@gmail.com>
2025-10-25 17:10:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~VRAg3AqS#UFJLZ9Loi-tdodAdEfWG28WQdQJlYqv-WmReIxn8DBg'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?EZZv2b'
 };
